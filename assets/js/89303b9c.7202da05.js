173801ebe64f71f174199f52be19a84bdd4cf8f5
dashagr <dasha.g1917@gmail.com>
2022-08-10 15:19:14 +0300

    Uploaded the first build vesion for a file

---
+++ assets/js/89303b9c.7202da05.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkinterkassa=self.webpackChunkinterkassa||[]).push([[17],{5745:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
